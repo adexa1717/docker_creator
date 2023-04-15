1154b5a3db4288c606cabe6a3f9599e002431c4c
Adilzhan <adexa343@gmail.com>
2023-04-15 15:01:12 +0600

    set up sending logs to aws

--- docker_creator/main.py
+++ docker_creator/main.py
@@ -1,6 +1,8 @@
-import docker
-
 import argparse
+
+import boto3
+import docker
+from docker.types import LogConfig
 
 
 def get_args():
@@ -11,22 +13,64 @@ def get_args():
     parser.add_argument("--aws-cloudwatch-group", dest="aws_group", type=str, help="A name of an AWS CloudWatch group")
     parser.add_argument("--aws-cloudwatch-stream", dest="aws_stream", type=str,
                         help="A name of an AWS CloudWatch stream")
-    parser.add_argument("--aws-access-key-id", dest="kwy_id", type=str, help="AWS access key ID")
+    parser.add_argument("--aws-access-key-id", dest="key_id", type=str, help="AWS access key ID")
     parser.add_argument("--aws-secret-access-key", dest="access_key", type=str, help="AWS secret access key")
     parser.add_argument("--aws-region", dest="region", type=str, help="A name of an AWS region")
     return parser.parse_args()
 
 
+def create_aws_client(aws_access_key_id, aws_secret_access_key, aws_region):
+    session = boto3.Session(
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        region_name=aws_region
+    )
+    return session.client("docker_creator_logs")
+
+
+def set_up_docker_container(args):
+    client = docker.from_env()
+    log_config = LogConfig(
+        type="awslogs",
+        config={
+            "awslogs-region": args.region,
+            "awslogs-group": args.aws_group,
+            "awslogs-stream": args.aws_stream,
+            "awslogs-create-group": "true",
+            "awslogs-multiline-pattern": "{timestamp_format} {log_level_pattern}"
+        }
+    )
+
+    container = client.containers.run(
+        args.docker_image,
+        args.bash_command,
+        detach=True,
+        log_config=log_config
+    )
+
+    return container.logs(stream=True, follow=True)
+
+
 def main():
     args = get_args()
-    # create a Docker client
-    client = docker.from_env()
+    aws_client = create_aws_client(
+        aws_access_key_id=args.key_id,
+        aws_secret_access_key=args.access_key,
+        aws_region=args.region
+    )
+    log_events = set_up_docker_container(args=args)
 
-    # create the Docker container with the specified image and command
-    container = client.containers.run(args.docker_image, args.bash_command, detach=True)
-
-    # print the container ID
-    print("Container ID:", container.id)
+    for log_event in log_events:
+        response = aws_client.put_log_events(
+            logGroupName=args.aws_group,
+            logStreamName=args.aws_stream,
+            logEvents=[
+                {
+                    "timestamp": int(log_event.attrs["timestamp"]),
+                    "message": log_event.decode("utf-8").strip()
+                }
+            ]
+        )
 
 
 if __name__ == "__main__":
